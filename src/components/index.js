798f6963c363a1a4260b34b42d8ce1855a759b39
Viktor Saranchuk <saranchukvm87@gmail.com>
2023-08-25 11:35:57 +0200

    Add Craco to resolve path aliasing issue

--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,4 @@
 export { default as HomePage } from "./HomePage/HomePage";
+export { default as Header } from "./Header/Header";
+export { default as MenuBar } from "./MenuBar/MenuBar";
+export { default as MenuItem } from "./MenuItem/MenuItem";
