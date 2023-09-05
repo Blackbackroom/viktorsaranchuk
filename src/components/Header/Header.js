a1a2d1c4547ce4988e0aab90ab928b770ea31895
Viktor Saranchuk <saranchukvm87@gmail.com>
2023-09-05 21:46:11 +0200

    add Name

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,9 +3,15 @@ import PhotoFrame from "@components/PhotoFrame/PhotoFrame";
 import { isMobile } from "react-device-detect";
 
 export default function Header() {
+  const name = "SARARNCHUK VIKTOR";
+  const title = "SALESFORCE DEVELOPER";
   return (
     <div className={`Header ${isMobile ? "HeaderMobile" : ""}`}>
-      <div className="NameWrapper"></div>
+      <div className={`NameSection ${isMobile ? "NameSectionMobile" : ""}`}>
+        <div className="Name">{name}</div>
+        <div className="Title">{title}</div>
+      </div>
+
       <PhotoFrame />
     </div>
   );
